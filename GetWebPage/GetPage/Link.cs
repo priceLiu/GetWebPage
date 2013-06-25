fa7eddd9e384a693292c9d8d43cad45beca5ad1d
price liu <liu_price168/2126.com>
2013-06-25 16:19:31 +0800

    first version

---
+++ GetWebPage/GetPage/Link.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GetWebPage
+{
+    public class Link
+    {
+        public string NavigateUrl
+        {
+            get;
+            set;
+        }
+
+        public string Text
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ GetWebPage/GetPage/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetPage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebPage webInfo = new WebPage("网址");
+
+            string context = webInfo.Context;//不包含html标签的所有内容  
+
+            string html = webInfo.M_html;//包含html标签的内容  
+        }
+    }
+}
